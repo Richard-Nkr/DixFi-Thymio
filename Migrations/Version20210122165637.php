0bdcbef159fd9c90d47548d64aedb3e2a286d63e
Rebecca <rebeccagnt@orange.fr>
2021-01-22 18:02:52 +0100

    Migrations bdd

---
+++ Migrations/Version20210122165637.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210122165637 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE challenge (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(30) NOT NULL, difficulty VARCHAR(30) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE chat (id INT AUTO_INCREMENT NOT NULL, id_teacher INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE children (id INT AUTO_INCREMENT NOT NULL, name_child VARCHAR(30) NOT NULL, teacher VARCHAR(30) NOT NULL, name_teacher VARCHAR(30) NOT NULL, first_name_teacher VARCHAR(30) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE comment (id INT AUTO_INCREMENT NOT NULL, content_comment LONGTEXT NOT NULL, created_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE `group` (id_teacher INT AUTO_INCREMENT NOT NULL, count_succeed INT NOT NULL, PRIMARY KEY(id_teacher)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE message (id INT AUTO_INCREMENT NOT NULL, id_user INT NOT NULL, id_chat INT NOT NULL, content_message LONGTEXT NOT NULL, date_message DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE private_challenge (id INT AUTO_INCREMENT NOT NULL, id_teacher INT NOT NULL, created_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE public_challenge (id INT AUTO_INCREMENT NOT NULL, name_correction_pdf VARCHAR(30) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE status (id INT AUTO_INCREMENT NOT NULL, id_group INT NOT NULL, id_challenge INT NOT NULL, status_int INT NOT NULL, count_help INT DEFAULT NULL, need_help TINYINT(1) NOT NULL, started_at DATETIME NOT NULL, finished_at DATETIME DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE thymio_challenge (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, nickname VARCHAR(30) NOT NULL, created_at DATETIME NOT NULL, deleted_at DATETIME DEFAULT NULL, password VARCHAR(150) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE challenge');
+        $this->addSql('DROP TABLE chat');
+        $this->addSql('DROP TABLE children');
+        $this->addSql('DROP TABLE comment');
+        $this->addSql('DROP TABLE `group`');
+        $this->addSql('DROP TABLE message');
+        $this->addSql('DROP TABLE private_challenge');
+        $this->addSql('DROP TABLE public_challenge');
+        $this->addSql('DROP TABLE status');
+        $this->addSql('DROP TABLE thymio_challenge');
+        $this->addSql('DROP TABLE user');
+    }
+}
